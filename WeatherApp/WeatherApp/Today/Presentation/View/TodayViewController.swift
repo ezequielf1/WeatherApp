a73a0218909eeda3d27f92af238ddb61836f6eaf
Ezequiel Fritz <ezequiel.fritz@reign.cl>
2021-09-14 10:59:32 +0200

    Implement app launchinng

--- WeatherApp/WeatherApp/Today/Presentation/View/TodayViewController.swift
+++ WeatherApp/WeatherApp/Today/Presentation/View/TodayViewController.swift
@@ -10,5 +10,6 @@ import UIKit
 final class TodayViewController: BaseViewController<TodayViewModel, Coordinator> {
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("asd")
     }
 }
